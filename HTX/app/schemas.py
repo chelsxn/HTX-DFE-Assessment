2aa4e4d2bb504d0cb4b6c9ebb54e8f846c5ea774
Chelson Chong <79626294+chelsxn@users.noreply.github.com>
2025-03-15 18:42:46 +0800

    Delete HTX directory

--- HTX/app/schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class ImageMetadata(BaseModel):
-    dimensions: str
-    file_format: str
-    file_size: int
-    date_time: str
-    # exif, etc. if needed
--- HTX/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Use chmod +x run.sh to make it executable
-
-# Activate virtual environment if needed
-# source venv/bin/activate
-
-# Run FastAPI on localhost:8000
-uvicorn app.main:app --host 127.0.0.1 --port 8000 --reload
